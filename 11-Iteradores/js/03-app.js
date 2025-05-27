01b7ed9f4cac8d03cdf9b2337846689fabbf3abc
Erick Ruiz <corragio.sage@gmail.com>
2025-05-27 16:35:38 -0600

    Cambios intermedios de hoy

--- 11-Iteradores/js/03-app.js
+++ 11-Iteradores/js/03-app.js
@@ -1,15 +1,21 @@
-// Fizz Buzz...
+// Sección 11 - Clase 82: Iteradores en JavaScript
+// Ejercicio Fizz Buzz...
 
+/*
+Fizz Buzz es un reto que complica mucho a los programadores, básicamente si un número es múltiplo de 3 imprimes  en consola FIZ,
+Si es múltiplo de 5 imprimes en consola BUZZ,
+Si es múltiplo de ambos como 15, 30, 45 debes imprimir FIZZ BUZZ
+*/
 // 3 6 9 12 15 ...  FIZZ
 // 5 10 15 20 25 ... BUZZ
 // 15 30 45 ... Fizz Buzz
 
-for(let i = 1; i < 100; i++  ) {
-    if(i % 15 === 0) {
-        console.log(`${i} FIZZ BUZZ`)
-    } else if(i % 3 === 0) {
-        console.log(`${i} fizz`);
-    } else if ( i % 5 === 0 ) {
-        console.log(`${i} buzz`)
-    } 
+for (let i = 1; i < 100; i++) {
+  if (i % 15 === 0) {
+    console.log(`${i} FIZZBUZZ!`);
+  } else if (i % 3 === 0) {
+    console.log(`${i} Fizz`);
+  } else if (i % 5 === 0) {
+    console.log(`${i} Buzz`);
+  }
 }
--- 11-Iteradores/js/04-app.js
+++ 11-Iteradores/js/04-app.js
@@ -12,6 +12,4 @@ while (i < 10) { // condicion
 }
 
 // El while se ejecuta mientras una condición sea verdadera, por lo tanto si inicicializamos en 20, no hará nada..
-
-
 /* Intenta realizar los mismos ejemplos  Detectar pares y nones y el fizz buzz con el while, seguramente va a quedar mucho mas claro... */
